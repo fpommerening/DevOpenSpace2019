d082b4ce4354c1ec64f8b4a7275e77550db2cb4d
Frank Pommerening <frank@pommerening-online.de>
2019-10-05 21:07:04 +0200

    Add build samples

---
+++ Build/Inside/WebApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace FP.DOS20219.BuildInside
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateWebHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
+}
